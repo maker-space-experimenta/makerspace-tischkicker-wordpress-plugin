839b217f78bd4c8bfe59a0aab0a16c4ead49bcc6
harmoniemand <harmoniemand@googlemail.com>
2019-02-28 00:00:44 +0100

    Disclaimer + Spielebestätigung Design

--- src/partials/page_main.php
+++ src/partials/page_main.php
@@ -12,6 +12,25 @@
 
 <link href="<?php echo plugins_url() . '/makerspace-tischkicker/src/assets/styles/main.css' ?>" rel="stylesheet">
 
+<div class="container-fluid mb-5">
+    <div class="row">
+        <div class="col">
+            <div class="card" style="max-width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title">Hinweise zur Spielaufzeichnung</h5>
+                    <p class="card-text">
+                        Der Spaß am Spiel steht immer im Vordergrund. Die Spieleaufzeichnung soll keine Rivalitäten oder Konkurenzen fördern.
+                        Sollten es zu extremen Konkurenzdenken bei den Spielenden kommen, behalten wir uns vor die Spieleaufzeichnung abzuschalten.
+                    </p>
+                    <p>
+                        Der Tischkicker soll immer ein Ort sein, an dem alle zusammen kommen und abschalten können.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="container-fluid mb-5" id="championships">
     <div class="row">
         <div class="col"><h2 class="text-uppercase">Laufende Wettbewerbe</h2></div>
@@ -25,6 +44,40 @@
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid mb-5" id="championships">
+    <div class="row">
+        <div class="col"><h2 class="text-uppercase">Meine unbestätigten Spiele</h2></div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action list-group-header">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="" style="width: 150px">Gegner*in</div>
+                        <div class="" style="width: 200px;">Tore Gegner*in</div>
+                        <div class="" style="width: 200px;">Meine Tore</div>
+                        <div class="" style="width: 50%;">Erfasst am</div>
+                        <div class="" style="width: 50%;">Aktionen</div>
+                    </div>
+                </a>
+
+                <a href="#" class="list-group-item list-group-item-action list-group-header">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="" style="width: 150px">Caro</div>
+                        <div class="" style="width: 200px;">10</div>
+                        <div class="" style="width: 200px;">5</div>
+                        <div class="" style="width: 50%;">27. 02. 2019 16:30</div>
+                        <div class="" style="width: 50%;">
+                            <button type="button" class="btn btn-outline-success btn-sm">Spiel ist korrekt</button>
+                            <button type="button" class="btn btn-outline-danger btn-sm">Spiel ist nicht korrekt</button>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
